d98afd0f692a124d701f089a6e4ca3308fe16cb4
晨杨 郑 <690574486@qq.com>
2019-01-28 21:49:48 +0800

    测试

--- Assets/Editor/AssetBundle/AssetBundleEditor.cs
+++ Assets/Editor/AssetBundle/AssetBundleEditor.cs
@@ -2,26 +2,52 @@
  * AssetBundle打包编辑
  */
 
-using System;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
-using Base.Debug;
+using System.Collections.Generic;
 
 public class AssetBundleEditor
 {
-    const string ABCONFIGPATH = "Assets/Editor/AssetBundle/ABConfig.asset";
-    public static readonly string CreateAssetBundlesPath = Application.streamingAssetsPath;
+    public const string ABCONFIGPATH = "Assets/Editor/AssetBundle/ABConfig.asset";
+    public static string CreateAssetBundlesPath = Application.streamingAssetsPath;
+    //==============================================================================
+    private static List<string> mAllFilePathList = new List<string>();
 
     [MenuItem("Tools/AssetBundle/Build")]
     public static void Build()
     {
+        mAllFilePathList.Clear();
+
         var abConfig = AssetDatabase.LoadAssetAtPath<ABConfig>(ABCONFIGPATH);
+
+        foreach (var dirPath in abConfig.DirectoryPathDict)
+        {
+            var dirInfo = new DirectoryInfo(dirPath.Value);
+            var files = dirInfo.GetFiles();
+            for (var i = 0; i < files.Length; i++)
+            {
+                var file = files[i];
+                if (file.Extension != ".meta")
+                {
+                    Debug.Log(file);
+                }
+            }
+            //mAllPathAB.Add(dirPath.Value);
+        }
+
         var prefabPathList = AssetDatabase.FindAssets("t:prefab", abConfig.PrefabDirPathList.ToArray());
         for (var i = 0; i < prefabPathList.Length; i++)
         {
-            Debugger.Log(AssetDatabase.GUIDToAssetPath(prefabPathList[i]));
+            var path = AssetDatabase.GUIDToAssetPath(prefabPathList[i]);
+            EditorUtility.DisplayProgressBar("查找Prefab", "Path: " + path, i * 1.0f / prefabPathList.Length);
+            var depends = AssetDatabase.GetDependencies(path);
+            for (var j = 0; j < depends.Length; j++)
+            {
+                //Debug.Log(depends[j]);
+            }
         }
+        EditorUtility.ClearProgressBar();
         AssetDatabase.Refresh();
     }
 
