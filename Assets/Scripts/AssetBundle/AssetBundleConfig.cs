3121422928fc89956b8735ab4a682b14d5823760
晨杨 郑 <690574486@qq.com>
2019-01-29 21:57:12 +0800

    AssetBundleConfig

---
+++ Assets/Scripts/AssetBundle/AssetBundleConfig.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * AssetBundle配置
+ */
+
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class AssetBundleConfig
+{
+    [XmlElement("AssetBundleList")]
+    public List<AssetBundleBase> AssetBundleList;
+}
+
+[System.Serializable]
+public class AssetBundleBase
+{
+    [XmlAttribute("MD5")]
+    public string MD5;
+    [XmlAttribute("Path")]
+    public string Path;
+    [XmlAttribute("ABName")]
+    public string ABName;
+    [XmlAttribute("AssetName")]
+    public string AssetName;
+    [XmlAttribute("ABDependList")]
+    public List<string> ABDependList;
+}
