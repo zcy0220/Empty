c9e226e0bf91a652b692612d1fad080f5f419e24
晨杨 郑 <690574486@qq.com>
2019-03-26 21:24:08 +0800

    android自动打包

---
+++ Assets/Editor/Build/BuildScript.cs
@@ -0,0 +1,53 @@
+﻿/**
+ * 构建打包
+ */
+
+using System;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class BuildScript
+{
+    /// <summary>
+    /// 打包的场景列表
+    /// </summary>
+    private static string[] GetBuildScenes()
+    {
+        List<string> names = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if (scene == null) continue;
+            if (scene.enabled) names.Add(scene.path);
+        }
+        return names.ToArray();
+    }
+
+    /// <summary>
+    /// 获得shell传入的参数
+    /// </summary>
+    /// <returns>The arguments.</returns>
+    private static Dictionary<string, string> GetArgs()
+    {
+        var args = new Dictionary<string, string>();
+        foreach (string arg in System.Environment.GetCommandLineArgs())
+        {
+            if (arg.StartsWith("@"))
+            {
+                var splitIndex = arg.IndexOf("=");
+                args.Add(arg.Substring(1, splitIndex - 1), arg.Substring(splitIndex + 1));
+            }
+        }
+        return args;
+    }
+
+    /// <summary>
+    /// Builds for android.
+    /// </summary>
+    public static void BuildForAndroid()
+    {
+        var args = GetArgs();
+        PlayerSettings.productName = args["name"];
+        PlayerSettings.bundleVersion = args["version"];
+        BuildPipeline.BuildPlayer(GetBuildScenes(), args["out"], BuildTarget.Android, BuildOptions.None);
+    }
+}
