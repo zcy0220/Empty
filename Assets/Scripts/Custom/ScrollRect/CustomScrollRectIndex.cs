b7b016cbb82a531b87a1b29f1d055f845e166918
zhengchenyang <690574486@qq.com>
2019-03-28 17:45:02 +0800

    自定义无限循环滚动列表

---
+++ Assets/Scripts/Custom/ScrollRect/CustomScrollRectIndex.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * 搭配自定义ScrollRect使用的索引
+ * 添加到CustomScrollRect中的Item要挂该组件
+ */
+
+using UnityEngine;
+
+public class CustomScrollRectIndex : MonoBehaviour
+{
+    /// <summary>
+    /// 索引
+    /// </summary>
+    private int mIndex;
+    /// <summary>
+    /// 父类的Scroll
+    /// </summary>
+    public CustomScrollRect Scroller { set; get; }
+
+    /// <summary>
+    /// 修改索引的话，修改对于的位置
+    /// </summary>
+    public int Index
+    {
+        get { return mIndex; }
+        set
+        {
+            mIndex = value;
+            transform.localPosition = Scroller.GetPosition(mIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/Custom/ScrollRect/CustomScrollRectTest.cs
@@ -0,0 +1,54 @@
+﻿/**
+ * 自定义ScrollRect测试
+ */
+
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CustomScrollRectTest : MonoBehaviour
+{
+    /// <summary>
+    /// 测试自定义的Scroll
+    /// </summary>
+    public CustomScrollRect Scroller;
+    /// <summary>
+    /// 模拟背包数据
+    /// </summary>
+    class Item { public int id; public int num; }
+    private List<Item> mItemList = new List<Item>();
+
+    /// <summary>
+    /// 模拟1000个背包数据
+    /// </summary>
+    private void Start()
+    {
+        for (var i = 0; i < 1000; i++)
+        {
+            mItemList.Add(new Item() { id = i, num = Random.Range(1, 1000) });
+        }
+        Scroller.Total = mItemList.Count;
+        Scroller.UpdateItem = UpdateItem;
+        Scroller.OnValueChanged(Vector2.zero);
+    }
+    
+    /// <summary>
+    /// 单项Item刷新数据
+    /// </summary>
+    /// <param name="item"></param>
+    private void UpdateItem(CustomScrollRectIndex item)
+    {
+        var index = item.Index;
+        var itemData = mItemList[index];
+        var id = item.transform.Find("Id").GetComponent<Text>();
+        id.text = itemData.id.ToString();
+        var num = item.transform.Find("Num").GetComponent<Text>();
+        num.text = itemData.num.ToString();
+        var addBtn = item.transform.Find("Add").GetComponent<Button>();
+        addBtn.onClick.RemoveAllListeners();
+        addBtn.onClick.AddListener(() => { Scroller.AddItem(index); });
+        var deleteBtn = item.transform.Find("Delete").GetComponent<Button>();
+        deleteBtn.onClick.RemoveAllListeners();
+        deleteBtn.onClick.AddListener(() => { Scroller.DeleteItem(index); });
+    }
+}
