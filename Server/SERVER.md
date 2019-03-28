de5ec3135825761ccefb112f64204cb4b180269c
zhengchenyang <690574486@qq.com>
2019-03-28 09:24:51 +0800

    protobuf服务器

--- Server/SERVER.md
+++ Server/SERVER.md
@@ -16,7 +16,11 @@ var message = root.lookupType("Proto.Example")
 
 const server = net.createServer((socket) => {
     socket.on('data', (buffer) => {
+        // 收到客户端消息
         console.log(message.decode(buffer))
+        // 响应一条数据测试
+        var respond = message.create({ ExampleInt: -1, ExampleFloat: -2.5, ExampleString: 'cba', ExampleArray: [ { ItemDouble: 1.2, ItemBool: true } ] })
+        socket.write(message.encode(respond).finish())
     })
 })
 
--- Server/server.js
+++ Server/server.js
@@ -8,12 +8,8 @@ const server = net.createServer((socket) => {
         // 收到客户端消息
         console.log(message.decode(buffer))
         // 响应一条数据测试
-        var respond = message.create({ ExampleInt: -1, ExampleFloat: -2.5, ExampleString: 'cba' })
-        // socket.write(message.encode(respond).finish())
-        var errMsg = message.verify(respond)
-        if (errMsg) throw Error(errMsg)
-        var buffer = new Buffer("Hell")
-        socket.write(buffer)
+        var respond = message.create({ ExampleInt: -1, ExampleFloat: -2.5, ExampleString: 'cba', ExampleArray: [ { ItemDouble: 1.2, ItemBool: true } ] })
+        socket.write(message.encode(respond).finish())
     })
 })
 
